ea29db7a5b9605a33dbd3aeabc824871a9e07fe2
Fernando Borretti <fernando@borretti.me>
2025-05-31 12:23:45 +1000

    More padding

--- HelloWorld/Source/main.swift
+++ HelloWorld/Source/main.swift
@@ -4,7 +4,7 @@ struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
             .font(.system(size: 32))
-            .padding(16)
+            .padding(64)
             // Do not allow shrinking the window below the minimum content size
             .fixedSize()
     }
