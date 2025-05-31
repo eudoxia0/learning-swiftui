a882e1105731d5ea08a1ad05b05aa9e42bde82c5
Fernando Borretti <fernando@borretti.me>
2025-05-31 12:18:56 +1000

    Add HelloWorld example

---
+++ HelloWorld/Source/main.swift
@@ -0,0 +1,22 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .font(.system(size: 32))
+            .padding(16)
+            // Do not allow shrinking the window below the minimum content size
+            .fixedSize()
+    }
+}
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        // Window starts at content size, can be resized up.
+        .windowResizability(.contentMinSize)
+    }
+}
